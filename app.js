a47ba7a491dc73fe86ad159116b579b02b40644a
AaronWard <aaronlaptop12@hotmail.com>
2017-10-25 15:02:58 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000))
+
+//serve static files in the public directory
+app.use(express.static('public'));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+//Process application/json
+app.use(bodyParser.json())
+
+// Index route -- HTML WILL GO HERE
+app.get('/', function (req, res) {
+	res.send('Hello world') 
+})
+
+
+
+//*************************************** */
+app.listen(app.get('port'), function () {
+	console.log('Application running on port', app.get('port'))
+})
