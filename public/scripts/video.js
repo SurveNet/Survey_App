5566286a08813ebc303423059d1d45d004ae48a6
AaronWard <aaronlaptop12@hotmail.com>
2017-10-27 11:19:40 +0100

    added video recording

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -1,18 +1,31 @@
 (function(){
-    var video = getElementById('video'),
+    var video = getElementById('video')
+    var canvas = getElementById('canvas')
+    var context = canvas.getContext('2d')
+
     vendURL = window.URL || window.webkitURL;
 
-    navigator.getMedia =    navigator.getUserMedia ||
+    navigator.getUserMedia =    navigator.getUserMedia ||
                             navigator.webkitGetUserMedia ||
                             navigator.mozGetUserMedia;
 
-    navigator.getMedia({
-        video: true, 
-        audio: false
-    }, function(stream){
-        video.src = vendURL.createObjectURL(Stream);
-        video.play();
-    }, function(error){
-        console.log(error);
-    });
+
+    if(navigator.getUserMedia){
+        navigator.getUserMedia({video: true}, stream, throwErr);
+    }
+
+    function stream(stream){
+        video.src = window.URL.createObjectURL(stream);
+    }
+    function throwErr(e){
+        alert(e.name);
+    }
+
+    function button(){
+        canvas.width = video.clientWidth;
+        canvas.height = video.clientHeight;
+        canvas.drawImage(video, 0, 0);
+
+    }
+
 })();
