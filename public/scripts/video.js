b8153a8902562c37dbe58a092d0e8a70f6a1aa9e
AaronWard <aaronlaptop12@hotmail.com>
2017-10-27 11:46:44 +0100

    Added dimensions to canvas

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -1,30 +1,34 @@
-    var video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d')
+if(document.readyState === 'interactive'){
 
-    vendURL = window.URL || window.webkitURL;
+        var video = document.getElementById('video');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d')
 
-    navigator.getUserMedia =    navigator.getUserMedia ||
-                                navigator.oGetUserMedia ||
-                                navigator.msGetUserMedia ||
-                                navigator.webkitGetUserMedia ||
-                                navigator.mozGetUserMedia;
+        vendURL = window.URL || window.webkitURL;
+
+        navigator.getUserMedia =    navigator.getUserMedia ||
+                                    navigator.oGetUserMedia ||
+                                    navigator.msGetUserMedia ||
+                                    navigator.webkitGetUserMedia ||
+                                    navigator.mozGetUserMedia;
 
 
-    if(navigator.getUserMedia){
-        navigator.getUserMedia({video: true}, streamCam, throwErr);
-    }
+        if(navigator.getUserMedia){
+            navigator.getUserMedia({video: true}, streamCam, throwErr);
+        }
 
-    function streamCam(stream){
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-    }
-    function throwErr(e){
-        alert(e.name);
-    }
+        function streamCam(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }
+        function throwErr(e){
+            alert(e.name);
+        }
 
-    function button(){
-        canvas.width = video.clientWidth;
-        canvas.height = video.clientHeight;
-        canvas.drawImage(video, 0, 0);
-    }
+        function button(){
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            canvas.drawImage(video, 0, 0);
+        }
+}
+    
