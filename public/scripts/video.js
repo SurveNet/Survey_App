71ecafa10d60a78438e5d1d074ea0720fd29ad2e
AaronWard <aaronlaptop12@hotmail.com>
2017-10-27 11:50:01 +0100

    Put script after canvas

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -1,34 +1,33 @@
-if(document.readyState === 'complete'){
 
-        var video = document.getElementById('video');
-        var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d')
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d')
 
-        vendURL = window.URL || window.webkitURL;
+vendURL = window.URL || window.webkitURL;
 
-        navigator.getUserMedia =    navigator.getUserMedia ||
-                                    navigator.oGetUserMedia ||
-                                    navigator.msGetUserMedia ||
-                                    navigator.webkitGetUserMedia ||
-                                    navigator.mozGetUserMedia;
+navigator.getUserMedia =    navigator.getUserMedia ||
+                            navigator.oGetUserMedia ||
+                            navigator.msGetUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia;
 
 
-        if(navigator.getUserMedia){
-            navigator.getUserMedia({video: true}, streamCam, throwErr);
-        }
-
-        function streamCam(stream){
-            video.src = window.URL.createObjectURL(stream);
-            video.play();
-        }
-        function throwErr(e){
-            alert(e.name);
-        }
-
-        function button(){
-            canvas.width = video.clientWidth;
-            canvas.height = video.clientHeight;
-            canvas.drawImage(video, 0, 0);
-        }
+if(navigator.getUserMedia){
+    navigator.getUserMedia({video: true}, streamCam, throwErr);
 }
+
+function streamCam(stream){
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+}
+function throwErr(e){
+    alert(e.name);
+}
+
+function button(){
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+    canvas.drawImage(video, 0, 0);
+}
+
     
