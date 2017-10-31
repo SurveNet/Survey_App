28f812bde8b733d6663dd9ef0254059242072807
Aaron <aaronlaptop12@hotmail.com>
2017-10-31 15:41:07 +0000

    Added tensorport token

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 module.exports = {
+  TENSORPORT_TOKEN : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTAwNTI2MTQsInVzZXJuYW1lIjoiYWFyb253YXJkIiwiZW1haWwiOiJhYXJvbmxhcHRvcDEyQGhvdG1haWwuY29tIiwidXNlcl9pZCI6NjM5fQ.QwbDhBY_cJBIsKb9WkVBecO6QmddZGo51Jydv6eA5Cw'
 };
