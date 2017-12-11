8a8ab0e35f9f86fee3b977c2959f05093c1f2a41
AaronWard <aaronlaptop12@hotmail.com>
2017-12-11 12:30:35 +0000

    logging base64

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -53,9 +53,13 @@ $(document).ready(function() {
     
         var img = null;
 
+
+    
         var canvasObj = document.getElementById("canvas");
         img = canvasObj.toDataURL();
 
+        console.log(img)
+    
         $.ajax({
             type: "POST",
             url:  "https://model-serve.herokuapp.com/model",
