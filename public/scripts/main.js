171ee3a93b1c68550eaab7692b7a473988c3212a
AaronWard <aaronlaptop12@hotmail.com>
2018-04-24 17:44:37 +0100

    updated link https

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -125,7 +125,7 @@ function record(){
             $.ajax({
                 type: "POST",
                 // url: "http://127.0.0.1:5000/api",
-                url: "http://servable.herokuapp.com/api",
+                url: "https://servable.herokuapp.com/api",
                 data: image, 
                 success: function(data){
                         console.log(data)
